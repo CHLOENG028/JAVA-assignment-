4eb80c6691ad43d99887547ceac9cbea92a77358
CHLOENG028 <xyng-wm24@student.tarc.edu.my>
2025-04-30 17:44:44 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,22 +1,62 @@
-public class Staff extends Person{
+public class Staff extends Person {
+    private char gender;
+    private String icNo;
     private double salary;
-    
-    public Staff(String id, String name, String phoneNo, char gender, String icNo, double salary){
-        super(id,name,phoneNo,gender,icNo);
+    private String role; 
+
+    public Staff() {
+        super("","","");
+        this.gender = 'U'; 
+        this.icNo = "";
+        this.salary = 0.0;
+        this.role = "Unknown";
+    }
+
+    public Staff(String role,String id, String name, String phoneNo, char gender, String icNo, double salary ) {
+        super(id,name,phoneNo);
+        this.role = role;
+        this.gender = gender;
+        this.icNo = icNo;
         this.salary = salary;
     }
 
-    public Staff() {
-        super("", "", "", ' ', "");
-        this.salary = 0.0;
+    public Staff(String id, String name, String phoneNo, char gender, String icNo, double salary ) {
+        super(id,name,phoneNo);
+        this.gender = gender;
+        this.icNo = icNo;
+        this.salary = salary;
     }
-    
+    public String getRole() {
+        return role;
+    }
 
-    public double getSalary(){
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getIcNo() {
+        return icNo;
+    }
+
+    public void setIcNo(String icNo) {
+        this.icNo = icNo;
+    }
+
+    public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary){
+    public void setSalary(double salary) {
         this.salary = salary;
     }
+
+
 }
